ccf46843e05fdf11d0538f46b40a406f4e466a63
Alexander <135832936+cena135@users.noreply.github.com>
2025-06-12 11:20:59 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesPage());
+}
+
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
+
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
